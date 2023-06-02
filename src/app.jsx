a3f785ef83bd9df67acc01070893cc2127f05b54
Denis F <denisf.salles@gmail.com>
2023-06-02 09:16:18 -0300

    chore: add example of preact + bpmn

---
+++ src/app.jsx
@@ -0,0 +1,44 @@
+import { useState, useEffect, useRef } from 'preact/hooks'
+import Modeler from 'bpmn-js/lib/Modeler';
+
+import customModdleExtension from './moddle/custom.json';
+// import PropertiesPanel from './properties-panel';
+
+export function App() {
+  const [count, setCount] = useState(0)
+  const modelerContainer = useRef()
+
+  function fetchDiagram(url) {
+    return fetch(url).then((response) => response.text());
+  }
+
+  useEffect(() => {
+    (async () => {
+      const $modelerContainer = document.querySelector('#modeler-container');
+      const diagramXML = await fetchDiagram("diagram.bpmn")
+
+      const modeler = new Modeler({
+        container: $modelerContainer,
+        moddleExtensions: {
+          custom: customModdleExtension
+        },
+        keyboard: {
+          bindTo: document.body
+        }
+      });
+      modeler.importXML(diagramXML);
+
+    })();
+  }, [])
+
+
+  return (
+    <>
+      <div style={{height: '100%'}}>
+        <h1>Preact</h1>
+        <div id='modeler-container'></div>
+      </div>
+    </>
+  )
+}
+
